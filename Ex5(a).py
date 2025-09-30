e78e458c12b7ea8abfc6efa70e449c72ca6f42d8
Munteanu Alexandru <provoda70GG@gmail.com>
2025-09-30 10:21:15 +0300

    Delete Ex5(a).py

--- Ex5(a).py
+++
@@ -1,4 +0,0 @@
-def suma_4(a1,b1,c1,d1):
-    return a1+b1+c1+d1
-a,b,c,d=int(input("a=")),int(input("b=")),int(input("c=")),int(input("d="))
-print("Suma numerelor ", a ,",", b ,",", c ," si ", d ," este:", suma_4(a,b,c,d))
