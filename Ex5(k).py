1c60ef16f45f2c1be79c26c815ae83f922f397f1
Munteanu Alexandru <provoda70GG@gmail.com>
2025-09-30 10:22:30 +0300

    Delete Ex5(k).py

--- Ex5(k).py
+++
@@ -1,9 +0,0 @@
-def zeci(n1):
-    N1=str(n1)
-    m=len(N1)
-    return m
-n = int(input('Introduce un număr: '))
-if n<=0:
-    print("Numarul trebuie sa fie pozitiv!")
-else:
-    print("Numarul de cifre in notatia zecimala al numarului intreg ", n, "este", zeci(n))
