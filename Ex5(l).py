5c71c4cb1119ecc8ed6638b803e75e6a5fec25c0
Munteanu Alexandru <provoda70GG@gmail.com>
2025-09-30 10:22:38 +0300

    Delete Ex5(l).py

--- Ex5(l).py
+++
@@ -1,9 +0,0 @@
-def sup(n1):
-    N1=str(n1)
-    m=len(N1)
-    return int(n1/pow(10,m-1))
-n = int(input('Introduce un număr: '))
-if n<=0:
-    print("Numarul trebuie sa fie pozitiv!")
-else:
-    print("Cifra superioara in notatia zecimala al numarului intreg ", n, "este", sup(n))
