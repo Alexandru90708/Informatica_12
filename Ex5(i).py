ada0d4a7ba8d72f23fe22d32bd159cc408cb5f5a
Munteanu Alexandru <provoda70GG@gmail.com>
2025-09-30 10:22:16 +0300

    Delete Ex5(i).py

--- Ex5(i).py
+++
@@ -1,5 +0,0 @@
-import math
-def multiplu (a7,b7):
-    return math.lcm(a7,b7)
-a,b=int(input('Introduce un numar:')),int(input('Introduce un numar:'))
-print("Cel mai mic multiplu comun al",a,"si",b,"este",multiplu(a,b))
