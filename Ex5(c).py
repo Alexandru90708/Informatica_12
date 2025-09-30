db5e70ed73bf10cf19ad798e9138c7029d2ac12a
Munteanu Alexandru <provoda70GG@gmail.com>
2025-09-30 10:21:42 +0300

    Delete Ex5(c).py

--- Ex5(c).py
+++
@@ -1,4 +0,0 @@
-def minimum(a3,b3,c3,d3):        
-    return min(a3,b3,c3,d3)
-a,b,c,d=int(input("a=")),int(input("b=")),int(input("c=")),int(input("d="))
-print("Cea mai mica valoare ale numerelor", a ,",", b ,",", c ," si ", d ," este:", minimum(a,b,c,d))
