f3c2ba67a52bfa4ef82ad9e3fd03165b3b6bfe25
Munteanu Alexandru <provoda70GG@gmail.com>
2025-11-02 13:53:33 +0200

    Update random_Ex5_MA.py

--- random_Ex5_MA.py
+++ random_Ex5_MA.py
@@ -1,2 +1,11 @@
 import random
-n=
+n=int(input("n="))
+k=[]
+s=0
+for i in range(1,n+1):
+    k.extend([random.randint(-100,100)])
+maxim=max(k)
+for x in k:
+    if x<maxim:
+        s+=x
+print("Suma elementelor mai mici ca elementul maxim",maxim,' al tabloului',k,' este',s)
