3391bd83dd88a8abdf47765e23fb11ad4988c076
Munteanu Alexandru <provoda70GG@gmail.com>
2025-11-09 22:07:46 +0200

    Pr_3_SET_MA.py

--- Pr_3_SET_MA.py
+++ Pr_3_SET_MA.py
@@ -1,19 +1,19 @@
 import random
-n=int(input('n='))
-X=set()
-Y=set()
-Z=set()
-for i in  range(n):
-    randx=random.randint(0,199)
-    randy=random.randint(0,199)
-    randz=random.randint(0,199)
-    X.add(randx)
-    Y.add(randy)
-    Z.add(randz)
-print(X)
-print(Y)
-print(Z)
-if not(X|Y|Z)==(not(X))&(not(Y))&(not(Z)) and not(X&Y&Z)==(not(X))|(not(Y))|(not(Z)):
-    print('Da negatiile sunt egale. Am demonstrat legile lui Morgan!')
+n = int(input('n='))
+X, Y, Z = set(), set(), set()
+U=set(range(200))
+for i in range(n):
+    X.add(random.randint(0,199))
+    Y.add(random.randint(0,199))
+    Z.add(random.randint(0,199))
+print("X =", X)
+print("Y =", Y)
+print("Z =", Z)
+lege1_stanga= U - (X | Y | Z)
+lege1_dreapta= (U - X) & (U - Y) & (U - Z)
+lege2_stanga= U - (X & Y & Z)
+lege2_dreapta= (U - X) | (U - Y) | (U - Z)
+if lege1_stanga == lege1_dreapta and lege2_stanga == lege2_dreapta:
+    print("\nDa, legile lui De Morgan sunt demonstrate corect!")
 else:
-    print('Nu am demostrat legile lui Morgan')
+    print("\nNu sunt demonstrate corect.")
