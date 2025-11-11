157defd05f20d2220db805d64d8b052e7a285eb7
Munteanu Alexandru <provoda70GG@gmail.com>
2025-11-11 11:11:30 +0200

    EVF_Tupluri_MA.py

--- EVF_Tupluri_MA.py
+++ EVF_Tupluri_MA.py
@@ -1,25 +1,29 @@
-data_curenta = (2025, 11, 11) 
-an_c, luna_c, zi_c = data_curenta
-produse = [
-    ("Lapte", 2025, 10, 10, 2025, 11, 10, 20.0),
-    ("Branza", 2025, 9, 1, 2025, 11, 20, 45.0),
-    ("Iaurt", 2025, 11, 1, 2025, 11, 12, 10.0),
-    ("Paine", 2025, 11, 9, 2025, 11, 13, 8.0),
-    ("Ulei", 2024, 11, 10, 2026, 11, 10, 30.0)
-]
-preturi_actuale = []
-expirate = []
-reducere50 = []
-reducere20 = []
-valabil_min1an = []
-valabil_max1luna = []
+data_curenta=(2025, 11, 11)
+an_c,luna_c,zi_c=data_curenta
+produse=[]
+with open("Produse.IN.txt", "r") as f:
+    for linie in f:
+        campuri = linie.split()
+        if len(campuri) == 8:
+            den = campuri[0]
+            af, lf, zf = int(campuri[1]), int(campuri[2]), int(campuri[3])
+            ae, le, ze = int(campuri[4]), int(campuri[5]), int(campuri[6])
+            pret = float(campuri[7])
+            produs = (den, af, lf, zf, ae, le, ze, pret)
+            produse.append(produs)
+preturi_actuale=[]
+expirate=[]
+reducere50=[]
+reducere20=[]
+valabil_min1an=[]
+valabil_max1luna=[]
 for p in produse:
     den, af, lf, zf, ae, le, ze, pret_init = p
-    zile_fab = af * 365 + lf * 30 + zf
-    zile_exp = ae * 365 + le * 30 + ze
-    zile_curent = an_c * 365 + luna_c * 30 + zi_c
-    zile_total = zile_exp - zile_fab
-    zile_ramase = zile_exp - zile_curent
+    zile_fab=af * 365 + lf * 30 + zf
+    zile_exp=ae * 365 + le * 30 + ze
+    zile_cur=an_c * 365 + luna_c * 30 + zi_c
+    zile_total=zile_exp - zile_fab
+    zile_ramase=zile_exp - zile_cur
     if zile_ramase < 0:
         pret_actual = 0
         expirate.append(den)
@@ -36,12 +40,15 @@ for p in produse:
         valabil_min1an.append(den)
     if zile_total <= 30:
         valabil_max1luna.append(den)
-print("=== PRODUSE ȘI PREȚURI ACTUALE ===")
+print('   Tabelul cu produse:')
+for x in produse:
+    print(x)
+print("   Produse și prețuri actuale:")
 for x in preturi_actuale:
-    print(f"{x[0]}: inițial {x[1]} lei, actual {round(x[2], 2)} lei")
-print("\n=== LISTE CERUTE ===")
-print("Produse expirate:", expirate)
-print("Reducere 50%:", reducere50)
-print("Reducere 20%:", reducere20)
-print("Valabile cel puțin 1 an:", valabil_min1an)
-print("Valabile cel mult 1 lună:", valabil_max1luna)
+    print(x[0],'inițial', x[1], 'lei, actual', round(x[2], 2), 'lei')
+print("\n    Liste cerute:")
+print("Produsele expirate:", expirate)
+print("Produsele cu reducere de 50%:", reducere50)
+print("Produsele cu reducere de 20%:", reducere20)
+print("Produsele valabile cel puțin 1 an:", valabil_min1an)
+print("produsele valabile cel mult 1 lună:", valabil_max1luna)
