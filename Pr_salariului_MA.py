cb72896f808ae846ba2a60d4588cd78af477bda7
Munteanu Alexandru <provoda70GG@gmail.com>
2025-12-02 11:09:31 +0200

    Pr_salariului_MA.py

--- Pr_salariului_MA.py
+++ Pr_salariului_MA.py
@@ -1,18 +1,28 @@
-def descompune_suma(lei):
-    suma = int(lei * 100)
-    valute = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1]
+def nr_bancnote(lei):
+    suma = int(lei)
+    bancnote = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
     rezultat = {}
-    for v in valute:
+    for v in bancnote:
         if suma >= v:
             nr = suma // v
             rezultat[v] = nr
             suma -= nr * v
     return rezultat
+def nr_monede(bani):
+    suma = (bani - int(bani))*100
+    monede=[50, 25, 10, 5, 1]
+    rezultat = {}
+    for v in monede:
+        if suma >= v:
+            nr = suma // v
+            rezultat[v] = int(nr)
+            suma -= nr * v
+    return rezultat
 salariu = float(input("Introduceți salariul: "))
-rezolvare = descompune_suma(salariu)
-print("\n")
-for i, nr in rezolvare.items():
-    if i >= 100:
-        print(f"{i // 100} lei : {nr}")
-    else:
-        print(f"{i} bani : {nr}")
+rezolvare_bancnote = nr_bancnote(salariu)
+rezolvare_monede =nr_monede(salariu)
+print("Salariul il vom primi in cel mai eficient mod in felul urmator:")
+for i, nr in rezolvare_bancnote.items():
+    print(f"{i} lei : {nr}")
+for i, nr in rezolvare_monede.items():
+    print(f"{i} bani : {nr}")
