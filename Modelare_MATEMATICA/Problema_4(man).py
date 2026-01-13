c707e909b8eedbc7a39f993bf16f5bf6529dfa31
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-13 10:59:03 +0200

    Problema_4(man).py

--- Modelare_MATEMATICA/Problema_4(man).py
+++ Modelare_MATEMATICA/Problema_4(man).py
@@ -1,1 +1,8 @@
-
+import math
+n=int(input('n='))
+an=0
+if n<0:
+    print('Valoarea lui n nu corespunde cerintei.')
+elif n>=0:
+    an=(pow((1+math.sqrt(5))/2,n+1)-pow((1-math.sqrt(5))/2,n+1))/math.sqrt(5)
+    print('Valoarea lui a de',n, 'este egala cu',int(an))
