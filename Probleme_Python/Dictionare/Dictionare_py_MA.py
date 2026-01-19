a5dd17b5ce4f8d1f11c5d5bafe092a1d9ba0a80d
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 20:33:58 +0200

    Create Dictionare_py_MA.py

---
+++ Probleme_Python/Dictionare/Dictionare_py_MA.py
@@ -0,0 +1,53 @@
+def dictionairy_1():    
+    key_value = {}        
+    key_value[2] = 56    
+    key_value[1] = 2    
+    key_value[5] = 12    
+    key_value[4] = 24    
+    key_value[6] = 18    
+    key_value[3] = 323
+    print('key_value', key_value)
+    for i in sorted(key_value.keys()):        
+        print(i, end=" ")
+    print('-cheile sortate in ordine crescatoare.')
+def dictionairy_2():    
+    key_value = {}        
+    key_value[2] = 56    
+    key_value[1] = 2    
+    key_value[5] = 12    
+    key_value[4] = 24    
+    key_value[6] = 18    
+    key_value[3] = 323
+    for i in sorted(key_value.keys()):        
+        print(i, end=" ")
+    print('-valorile sortate in ordine crescatoare dupa cheie (implicit).')
+def dictionairy_3():
+    key_value = {}        
+    key_value[2] = 56    
+    key_value[1] = 2    
+    key_value[5] = 12    
+    key_value[4] = 24    
+    key_value[6] = 18    
+    key_value[3] = 323
+
+    for k in sorted(key_value, key=lambda x: key_value[x]):
+        print(key_value[k], end=" ")
+    print("-valorile sortate in ordine crescatoare dupa valoare.")
+def dictionairy_4():
+    key_value = {}        
+    key_value[2] = 56    
+    key_value[1] = 2    
+    key_value[5] = 12    
+    key_value[4] = 24    
+    key_value[6] = 18    
+    key_value[3] = 323
+
+    rezultat = sorted(key_value.items(), key=lambda x: x[1])
+    print(rezultat,"-lista tuplurilor sortata dupa valoare.")
+def main():   
+    dictionairy_1()
+    dictionairy_2()
+    dictionairy_3()
+    dictionairy_4()
+if __name__ == "__main__":    
+    main()
