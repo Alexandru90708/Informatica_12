cdc3b71d8a0af53cbbd6e629df4dc184c7aac1bf
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 21:36:38 +0200

    Create EX1_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX1_MA.py
@@ -0,0 +1,4 @@
+s=float(input("Introduceți distanța pe care trebuie să o parcurgem(în kilometri):"))
+v=float(input("Introduceți constantă cu care ne vom deplasa(în km/h):"))
+t=s/v
+print('Distanța de',s,'km va fi parcursă în',t,'ore.')
