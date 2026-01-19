6474a99fcdff8b1cfba1d48a3a3b5ce8a9b534f1
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 22:06:45 +0200

    Create EX6_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX6_Modelare_mate_MA.py
@@ -0,0 +1,4 @@
+A=float(input("Introduceți suprafață(ha):"))
+d=float(input("Introduceți doză(kg/ha):"))
+Q=A*d
+print('Pentru o suprafață de',A,'ha este necesar o cantitate de',Q,'kg de îngrășăminte, cu doza de',d,'(kg/ha).')
