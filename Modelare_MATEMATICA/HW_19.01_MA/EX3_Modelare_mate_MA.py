720da12baa544e0c00c2661864e5b1a267d257c0
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 21:49:32 +0200

    Create EX3_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX3_Modelare_mate_MA.py
@@ -0,0 +1,4 @@
+P=int(input("Introduceți putere (kW):"))
+t=int(input("Introduceți timp de funcționare (h):"))
+E=P*t
+print('Aparatul folosit timp de',t,'ore, cu puterea de',P,'kW, consumă',E,'kWh.')
