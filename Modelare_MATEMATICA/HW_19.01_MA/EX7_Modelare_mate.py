e40c1721d85858ce30e58997920b4fbb66ec8b0c
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 22:22:02 +0200

    Create EX7_Modelare_mate.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX7_Modelare_mate.py
@@ -0,0 +1,5 @@
+C1=float(input("Introduceți concentrația inițială:"))
+V1=float(input("Introduceți volumul inițial:"))
+V2=float(input("Introduceți volumul final:"))
+C2=C1*V1/V2
+print('Dacă soluția inițială cu volumul de',V1,'are concentrația',C1,',iar mai apoi soluția este diluată la volumul',V2,',atunci concentrția devine.',C2)
