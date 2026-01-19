7bb4c83a26344d7d3648753356fea17312d04908
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 22:26:21 +0200

    Create EX8_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX8_Modelare_mate_MA.py
@@ -0,0 +1,4 @@
+p=int(input("Introduceți productivitate(unități/oră):"))
+t=int(input("Introduceți timp de lucru(ore):"))
+N=p*t
+print('Într-o periodă de',t,'ore, cu productivitatea de',p,'unități/oră, se vor produce',N,'de unități.')
