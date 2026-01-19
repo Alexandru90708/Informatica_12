2b431c04600338777c525bb9b7c53e3452bc9fae
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 21:54:21 +0200

    Create EX4_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/EX4_Modelare_mate_MA.py
@@ -0,0 +1,4 @@
+p=int(input("Introduceți prețul inițial:"))
+r=int(input("Introduceți reducere procentuală (%):"))
+C=p*(1-r/100)
+print('Prețul produsul de',p,'lei, după o reducere de',r,'%, va fi de',C,'lei.')
