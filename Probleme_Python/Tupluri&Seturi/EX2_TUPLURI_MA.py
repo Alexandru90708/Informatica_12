6c84315790688b5e74d88151f2185bd460e781df
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 20:39:21 +0200

    Update EX2_TUPLURI_MA.py

--- Probleme_Python/Tupluri&Seturi/EX2_TUPLURI_MA.py
+++ Probleme_Python/Tupluri&Seturi/EX2_TUPLURI_MA.py
@@ -1,21 +1,10 @@
-elev1 = ("Popescu", "Ion", "M", 8, 9, 7)
-elev2 = ("Ionescu", "Maria", "F", 10, 10, 10)
-elev3 = ("Georgescu", "Andrei", "M", 4, 6, 5)
-elev4 = ("Dumitrescu", "Elena", "F", 9, 9, 9)
-elev5 = ("Vasilescu", "Mihai", "M", 7, 8, 6)
-elevi = [elev1, elev2, elev3, elev4, elev5]
-
-print("Notele medii:")
-for elev in elevi:
-    medie = sum(elev[3:6]) / 3
-    print(elev[0], elev[1], 'are media:', round(medie, 2))
-
-print("\nRestanțieri:")
-for elev in elevi:
-    if elev[3] < 5 or elev[4] < 5 or elev[5] < 5:
-        print(elev[0], elev[1], "este restanțier.")
-
-print("\nEminenti:")
-for elev in elevi:
-    if elev[3] >= 9 and elev[4] >= 9 and elev[5] >= 9:
-        print(elev[0], elev[1], "este eminent")
+data=(9, 11, "duminică")
+zile=["miercuri", "joi", "vineri", "sâmbătă", "duminică", "luni","marți"]
+zi_an=313
+for i in range(7):
+    if zile[i]==data[2]:
+        if zi_an%7>i:
+            nr=zi_an//7+1
+        else:
+            nr=zi_an//7
+        print("Au fost",nr,"zile de",data[2])
