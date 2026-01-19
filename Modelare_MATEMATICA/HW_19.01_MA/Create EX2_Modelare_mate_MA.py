45e13021fecafdb47de8bd6045ff00ce0300ef0a
Munteanu Alexandru <provoda70GG@gmail.com>
2026-01-19 21:41:45 +0200

    Create Create EX2_Modelare_mate_MA.py

---
+++ Modelare_MATEMATICA/HW_19.01_MA/Create EX2_Modelare_mate_MA.py
@@ -0,0 +1,4 @@
+a=int(input("Introduceți suma economisită pe lună:"))
+n=int(input("Introduceți numarul de luni:"))
+S=a*n
+print('Dacă economisim timp de',n,'luni câte',a,'de lei în fiecare lună, vom aduna',S,'de lei.')
